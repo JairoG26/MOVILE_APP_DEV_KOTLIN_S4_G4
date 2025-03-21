8143b343a6f777ec3b72b7aa8f5ae51300b7ed2e
AndresCarrasquillag <111008783+AndresCarrasquillag@users.noreply.github.com>
2025-03-21 17:43:44 -0500

    SignUp Fully integrated with backend (#10)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.lastbite"
-        minSdk = 27 // Samsung Galaxy A8
+        minSdk = 31 // Samsung Galaxy A8
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
