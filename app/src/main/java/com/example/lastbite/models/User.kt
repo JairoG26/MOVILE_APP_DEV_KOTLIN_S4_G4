b71a0e9558ebb9972d2c07d2199cef3d6b595947
AndresCarrasquillag <j.carrasquillag@uniandes.edu.co>
2025-03-21 15:27:17 -0500

    SignUp Fully integrated with backend

--- app/src/main/java/com/example/lastbite/models/User.kt
+++ app/src/main/java/com/example/lastbite/models/User.kt
@@ -1,12 +1,12 @@
 package com.example.lastbite.models
 
 data class User(
-    val user_id: Int,
-    val area_id: Int,
-    val description: String,
-    val mobileNumber: String,
+    //val user_id: Int,
+    val area_id: Int?,
+    val description: String?,
+    val mobile_number: String?,
     val name: String,
-    val email: String,
-    val userType: String,
-    val verificationCode: Int
+    val user_email: String,
+    val user_type: String,
+    val verification_code: Int?
 )
