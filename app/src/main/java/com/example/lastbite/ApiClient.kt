192a272fc2cbb31e56e5908a4cf1060671d02ecc
AndresCarrasquillag <j.carrasquillag@uniandes.edu.co>
2025-04-09 20:41:37 -0500

    Nearby working

--- app/src/main/java/com/example/lastbite/ApiClient.kt
+++ app/src/main/java/com/example/lastbite/ApiClient.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
 
-    private const val BASE_URL = "http://192.168.80.17:5000/api/"
+    private const val BASE_URL = "https://9f7c-186-113-84-35.ngrok-free.app/api/"
 
     val instance: Retrofit by lazy {
         Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
