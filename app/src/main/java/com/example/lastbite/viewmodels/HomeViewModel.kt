240b1f436923d193557709dddd4393b9f8bedb6e
Pipelanche <j.lancherosc@uniandes.edu.co>
2025-04-26 01:34:03 -0500

    Eventual connectivity for Home

---
+++ app/src/main/java/com/example/lastbite/viewmodels/HomeViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.lastbite.viewmodels
+
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+}
