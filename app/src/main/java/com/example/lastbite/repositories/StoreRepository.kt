6e0b266b43d4a0551add791736333f6492e7eb50
AndresCarrasquillag <j.carrasquillag@uniandes.edu.co>
2025-05-03 12:47:01 -0500

    Nearby uses backend

--- app/src/main/java/com/example/lastbite/repositories/StoreRepository.kt
+++ app/src/main/java/com/example/lastbite/repositories/StoreRepository.kt
@@ -59,4 +59,22 @@ class StoreRepository {
             }
         })
     }
+
+    fun fetchNearbyStores(lat: Double, lon: Double, callback: (List<Store>?) -> Unit) {
+        apiService.getNearByStores(lat, lon).enqueue(object : Callback<List<Store>> {
+            override fun onResponse(call: Call<List<Store>>, response: Response<List<Store>>) {
+                if (response.isSuccessful) {
+                    callback(response.body())
+                } else {
+                    callback(null)
+                }
+            }
+
+            override fun onFailure(call: Call<List<Store>>, t: Throwable) {
+                callback(null)
+            }
+        })
+    }
+
+
 }
