0f8cc9a0c524aee816ac473854d852dcf939cc38
AndresCarrasquillag <j.carrasquillag@uniandes.edu.co>
2025-05-20 18:06:53 -0500

    Nearby uses backend

--- app/src/main/java/com/example/lastbite/ApiClient.kt
+++ app/src/main/java/com/example/lastbite/ApiClient.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
 
-    private const val BASE_URL = "http://157.253.162.40:5000/api/"
+    private const val BASE_URL = "https://baf8-181-237-22-203.ngrok-free.app/api/"
 
     val instance: Retrofit by lazy {
         Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
